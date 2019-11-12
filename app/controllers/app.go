895b1f23848d835f8fd5ea997449dfb4beba1ed1
Mike Kinney <mike.kinney@gmail.com>
2019-11-12 10:33:38 -0800

    inc fiat input, add date/time, reformat confirm page

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"encoding/json"
 	"net/url"
+	"time"
 	"net/http"
 	"crypto/tls"
 )
@@ -164,7 +165,7 @@ func (c App) Index() revel.Result {
 }
 
 // hidden_ fields should be blank incoming
-func (c App) RemoteSell(location string, crypto string, fiat float64, hidden_uuid string, hidden_crypto_amount  string, hidden_minutes string, hidden_address string) revel.Result {
+func (c App) RemoteSell(location string, crypto string, fiat float64, hidden_uuid string, hidden_crypto_amount  string, hidden_minutes string, hidden_address string, hidden_now time.Time) revel.Result {
 	c.Validation.Required(location).Message("Location is a required field.")
 	//revel.AppLog.Debug("%+v", c.Validation)
 
@@ -228,10 +229,11 @@ func (c App) RemoteSell(location string, crypto string, fiat float64, hidden_uui
 	hidden_crypto_amount = fmt.Sprintf("%f", crypto_amount)
 	hidden_minutes = fmt.Sprintf("%d", sr.ValidityInMinutes)
 	hidden_address = sr.CryptoAddress
+	hidden_now = time.Now()
 	err := qrcode.WriteFile(qrString, qrcode.Medium, 256, "public/img/rs_" + hidden_uuid + ".png")
 	if err != nil {
 		revel.AppLog.Error("Could not write qrcode", err)
 	}
 
-	return c.Render(location, crypto, fiat, hidden_uuid, hidden_crypto_amount, hidden_minutes, hidden_address)
+	return c.Render(location, crypto, fiat, hidden_uuid, hidden_crypto_amount, hidden_minutes, hidden_address, hidden_now)
 }
